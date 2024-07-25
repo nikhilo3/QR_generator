6095f2efeef90f1bac942275cbde0a6fdf7d5e51
nikhilo3 <nikhilladani03@gmail.com>
2024-07-25 17:27:45 +0530

    download qr code

--- Server/Router/qrRoute.js
+++ Server/Router/qrRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { getQr } from "../Controller/qrCtrl.js";
-import { downloadQr } from "../Controller/downloadQrCtrl.js";
+// import { downloadQr } from "../Controller/downloadQrCtrl.js";
 
 const Qrrouter = express.Router();
 
 Qrrouter.post('/generateqr',getQr);
-Qrrouter.post('/downloadqr',downloadQr);
+// Qrrouter.post('/downloadqr',downloadQr);
 
 export default Qrrouter;
